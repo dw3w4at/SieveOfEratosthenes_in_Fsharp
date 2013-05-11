c5547a92144b978f64891d760404d22c0e1e217c
Youhei Kondou <dw3w4at@ovi.com>
2013-05-11 13:07:16 +0900

    Delete unnessesory comments

--- Prime.fs
+++ Prime.fs
@@ -17,21 +17,3 @@
                        | _ -> Some(current, (next, next + 2L))
                      ) (11L, 13L)
   let sieveOfEratosthenes = Seq.choose isPrimeElement seedSeq
- (*let sieveOfEratosthenes = seq {
-    let primeTable = ref (Seq.ofList [2L ;3L ;5L ;7L])
-    let sieve'sSeed = [11L; 13L; 17L; 19L]
-    let takeSmallerThanSquare n sequence =
-      Seq.takeWhile (fun elem -> elem * elem <= n) sequence
-    let existsMultiple n sequence =
-      Seq.exists (fun elem -> n % elem = 0L) sequence
-    let sieve digit =
-      let isPrimeElement seed =
-        let elem = digit * 10L + seed
-        match (existsMultiple elem <| takeSmallerThanSquare elem !primeTable) with
-        | true -> None
-        | false -> Some(elem)
-      primeTable := Seq.append !primeTable (Seq.choose isPrimeElement sieve'sSeed)
-    List.iter (fun i -> sieve i) [0L..500L]
-    yield! !primeTable
-  };;*)
-Seq.last Prime.sieveOfEratosthenes;;
